cbe5e76417c5d7722dc3dcf6fc240baa2ffcd1be
JulianaAmoasei <late.parrot@gmail.com>
2023-08-25 12:48:04 -0300

    fix: aspas app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/teste", (req, res) => {
+app.get('/teste', (req, res) => {
   res
     .status(200)
-    .send({ mensagem: "boas-vindas à API" });
+    .send({ mensagem: 'boas-vindas à API' });
 });
 
 module.exports = app;
