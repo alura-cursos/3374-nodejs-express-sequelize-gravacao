b816ea2e0d37153ffc4c7f87f3d15aba4ba2ca8c
JulianaAmoasei <late.parrot@gmail.com>
2023-09-01 09:49:09 -0300

    video: adicionando services e controllers

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const pessoas = require('./pessoasRoute.js');
+const categorias = require('./categoriasRoute.js');
 
 module.exports = app => {
   app.use(
     express.json(),
     pessoas,
+    categorias
   );
 };
